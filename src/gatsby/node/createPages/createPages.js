09eb1514119436241234e2f48f244c5f8e7da226
Mikko Ikonen <mikko.ikonen@eficode.com>
2021-07-14 16:06:43 +0300

    Linter

--- src/gatsby/node/createPages/createPages.js
+++ src/gatsby/node/createPages/createPages.js
@@ -55,25 +55,23 @@ module.exports = async function createPages({ actions, graphql }) {
 
   return graphql(`
     query {
-      all:
-        allMarkdownRemark(limit: 1000) {
-          edges {
-            node {
-              id
-              fields {
-                slug
-              }
-              frontmatter {
-                tags
-                templateKey
-                sportKey
-                product_category
-              }
+      all: allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              tags
+              templateKey
+              sportKey
+              product_category
             }
           }
         }
       }
-
+    }
   `).then((result) => {
     if (result.errors) {
       result.errors.forEach((e) => console.error(e.toString()))
@@ -83,15 +81,14 @@ module.exports = async function createPages({ actions, graphql }) {
     const posts = result.data.all.edges
 
     posts.forEach((edge) => {
-      const id = edge.node.id
-      console.log("Path: " + edge.node.fields.slug)
+      const { id } = edge.node
+      console.log(`Path: ${edge.node.fields.slug}`)
       createPage({
-
         path: edge.node.fields.slug,
         tags: edge.node.frontmatter.tags,
         component: path.resolve(
           // `src/components/templates/${String(edge.node.frontmatter.templateKey)}.js`
-          `src/components/templates/SkuDetailTemplate/SkuDetailTemplate.js`
+          `src/components/templates/SkuDetailTemplate/SkuDetailTemplate.js`,
         ),
         // additional data can be passed via context
         context: {
@@ -101,56 +98,53 @@ module.exports = async function createPages({ actions, graphql }) {
     })
 
     // Create sportCategories:
-    let sportCategories = []
+    const sportCategories = []
 
     // Iterate through each product, putting all found sport/category into `sportCategories`
     posts.forEach((edge) => {
       if (_.get(edge, `node.frontmatter.sportKey`)) {
-        console.log("Page: " + edge.node.frontmatter.sportKey)
-        console.log("sportCategories: " + sportCategories)
-
-
+        console.log(`Page: ${edge.node.frontmatter.sportKey}`)
+        console.log(`sportCategories: ${sportCategories}`)
 
         if (_.get(edge, `node.frontmatter.product_category`)) {
           if (!(edge.node.frontmatter.sportKey in sportCategories)) {
-            sportCategories[edge.node.frontmatter.sportKey] = [edge.node.frontmatter.product_category];
+            sportCategories[edge.node.frontmatter.sportKey] = [edge.node.frontmatter.product_category]
             // sportCategories.push({
             //     key:   edge.node.frontmatter.sportKey,
             //     value: [edge.node.frontmatter.product_category]
             // });
-            console.log("if1 sportCategories: " + sportCategories)
-            console.log("if1 sportCategories: " + sportCategories[edge.node.frontmatter.sportKey][0])
+            console.log(`if1 sportCategories: ${sportCategories}`)
+            console.log(`if1 sportCategories: ${sportCategories[edge.node.frontmatter.sportKey][0]}`)
           } else {
-            console.log("if2 sportCategories: " + sportCategories)
-            console.log("if2 sportCategories: " + sportCategories[edge.node.frontmatter.sportKey])
+            console.log(`if2 sportCategories: ${sportCategories}`)
+            console.log(`if2 sportCategories: ${sportCategories[edge.node.frontmatter.sportKey]}`)
             console.log(sportCategories[edge.node.frontmatter.sportKey])
-            sportCategories[edge.node.frontmatter.sportKey].push(edge.node.frontmatter.product_category);
+            sportCategories[edge.node.frontmatter.sportKey].push(edge.node.frontmatter.product_category)
             // sportCategories.push({
             //     key:   edge.node.frontmatter.sportKey,
             //     value: edge.node.frontmatter.product_category
             // });
             // sportCategories = sportCategories.concat(edge.node.frontmatter.sportKey + "/" + edge.node.frontmatter.product_category)
-            console.log("Page: " + edge.node.frontmatter.sportKey + "/" + edge.node.frontmatter.product_category)
+            console.log(`Page: ${edge.node.frontmatter.sportKey}/${edge.node.frontmatter.product_category}`)
             console.log(sportCategories)
           }
-
-          }
+        }
         // sportCategories = sportCategories.concat(edge.node.frontmatter.sportKey+"/mikko")
       }
     })
 
     for (sportCategory in sportCategories) {
-      console.log(sportCategory, sportCategories[sportCategory]);
+      console.log(sportCategory, sportCategories[sportCategory])
 
       // Eliminate duplicate sportCategories
       sportCategories[sportCategory] = _.uniq(sportCategories[sportCategory])
-      console.log(sportCategory, sportCategories[sportCategory]);
+      console.log(sportCategory, sportCategories[sportCategory])
 
       // Make category pages
-      sportCategories[sportCategory] .forEach((productCategory) => {
+      sportCategories[sportCategory].forEach((productCategory) => {
         const categoryPath = `/${sportCategory}/product/${productCategory}/`
-        console.log("SportCategory: " + sportCategory);
-        console.log("productCategory: " + productCategory);
+        console.log(`SportCategory: ${sportCategory}`)
+        console.log(`productCategory: ${productCategory}`)
 
         // TODO: enable:
         // createPage({
@@ -162,9 +156,6 @@ module.exports = async function createPages({ actions, graphql }) {
         //   },
         // })
       })
-
     }
-
-
   })
 }
